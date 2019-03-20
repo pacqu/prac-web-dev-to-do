d58cc83abc92ec10a85f4442f8ea4bdfd97e75b2
Justin Pacquing <pacqqu@gmail.com>
2019-03-19 22:36:05 -0400

    react app now done

--- to-do-react/src/App.js
+++ to-do-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cookies from 'js-cookie';
 import logo from './logo.svg';
 import './App.css';
 import './main.css';
@@ -10,13 +11,87 @@ class App extends Component {
     super();
     this.state = {
       loggedIn: false,
+      toDoList: []
     };
   }
 
   componentDidMount(){
-    axios.get('/loggedIn')
+    if (Cookies.get('token')){
+      this.setState({loggedIn: true});
+      this.getToDoList();
+    }
+  }
+
+  handleSubmitRegisterInfo(){
+    /* To-Do: Give user feedback that login is being attempted */
+    let username = this.refs.username_entry.value;
+    axios.post('https://hunter-todo-api.herokuapp.com/user', { username: username})
     .then(res => {
-      if (res.data.username) this.setState({loggedIn: true, username: res.data.username});
+      axios.post('https://hunter-todo-api.herokuapp.com/auth', { username: username})
+      .then(res => {
+        this.refs.username_entry.value = "";
+        if (res.data.token){
+          Cookies.set('token', res.data.token);
+          Cookies.set('username', username);
+          this.setState({loggedIn: true, message: ""});
+          this.getToDoList();
+        }
+        console.log(res);
+      })
+      .catch(err => {
+        this.refs.username_entry.value = "";
+        this.setState({message: "Login Failed! User doesn't exist"})
+        console.log(err);
+      })
+    })
+    .catch(err => {
+      this.refs.username_entry.value = "";
+      this.setState({message: "Registration Failed! User already exists"});
+    })
+  }
+
+  handleSubmitLoginInfo(){
+    /* To-Do: Give user feedback that login is being attempted */
+    let username = this.refs.username_entry.value;
+    axios.post('https://hunter-todo-api.herokuapp.com/auth', { username: username})
+    .then(res => {
+      this.refs.username_entry.value = "";
+      if (res.data.token){
+        Cookies.set('token', res.data.token);
+        Cookies.set('username', username);
+        this.setState({loggedIn: true, message: ""});
+        this.getToDoList();
+      }
+      console.log(res);
+    })
+    .catch(err => {
+      this.refs.username_entry.value = "";
+      this.setState({message: "Login Failed! User doesn't exist"})
+      console.log(err);
+    })
+  }
+
+  getToDoList(){
+    axios.get('https://hunter-todo-api.herokuapp.com/todo-item', { headers: {'sillyauth': Cookies.get('token')} })
+    .then(res => {
+      this.setState({
+        toDoList: res.data
+      })
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  toggleComplete(taskId, completed){
+    axios.put('https://hunter-todo-api.herokuapp.com/todo-item/' + taskId,
+    {'completed': !completed},
+    {headers: {
+      'sillyauth': Cookies.get('token')
+    }})
+    .then(res => {
+      this.getToDoList();
       console.log(res);
     })
     .catch(err => {
@@ -24,42 +99,107 @@ class App extends Component {
     });
   }
 
-  handleSubmitLoginInfo(event){
-    axios.post('/logIn', { username: this.refs.username_entry.value})
+  handleDelete(taskId, completed){
+    axios.delete('https://hunter-todo-api.herokuapp.com/todo-item/' + taskId,
+    {headers: {
+      'sillyauth': Cookies.get('token')
+    }})
     .then(res => {
-      this.refs.username_entry.value = "";
-      if (res.data.username) this.setState({loggedIn: true, username: res.data.username});
+      this.getToDoList();
       console.log(res);
     })
     .catch(err => {
-      this.refs.username_entry.value = "";
       console.log(err);
     });
   }
 
-  handleLogout(event){
-    axios.get('/logout')
+  handleNewTask(){
+    /* To-Do: Give user feedback that login is being attempted */
+    let task = this.refs.task_entry.value;
+    axios.post('https://hunter-todo-api.herokuapp.com/todo-item',
+    { content: task },
+    {headers: { 'sillyauth': Cookies.get('token')} }
+  )
     .then(res => {
-      if (!(res.data.loggedIn)) this.setState({loggedIn: false, username: ""});
+      this.refs.task_entry.value = "";
+      this.getToDoList();
       console.log(res);
     })
     .catch(err => {
+      this.refs.task_entry.value = "";
       console.log(err);
-    });
+    })
+  }
+
+  handleLogout(){
+    Cookies.remove('token');
+    Cookies.remove('username');
+    this.setState({loggedIn: false, toDoList: []});
+
   }
 
   render() {
     let header = (<h1> To-Do List</h1>);
+    let message = (<div> </div>)
+    if (this.state.message) message = (<h5>{this.state.message}</h5>)
     let body = (<div>
       <input name="username" type="text" placeholder="username" ref="username_entry"></input>
       <br></br>
-      <button type="button" name="submit">Register</button>
-      <button type="button" name="submit" onClick={this.handleSubmitLoginInfo.bind(this)}>Log In</button>
+      <button type="button" name="submit" onClick={() => this.handleSubmitRegisterInfo()}>Register</button>
+      <button type="button" name="submit" onClick={() => this.handleSubmitLoginInfo()}>Log In</button>
+      {message}
     </div>);
     let logout;
     if (this.state.loggedIn){
-      header = (<h1> {this.state.username}{"'s"} To-Do List</h1>);
-      logout = (<button type="button" name="submit" onClick={this.handleLogout.bind(this)}>Logout</button>);
+      let toDoList = this.state.toDoList;
+      header = (<h1> {Cookies.get('username')}{"'s"} To-Do List</h1>);
+      if (toDoList.length){
+        let toDoRows = toDoList.map((task,i) => {
+          let completed = '';
+          let content = task.content;
+          let button = 'Done';
+          if (task.completed){
+            completed = 'X';
+            content = (<s>{task.content}</s>);
+            button = 'Undone';
+          }
+          return (
+            <tr key={i}>
+              <td>{completed}</td>
+              <td>{content}</td>
+              <td><button onClick={()=>this.toggleComplete(task.id, task.completed)}>{button}</button></td>
+              <td><button onClick={()=>this.handleDelete(task.id, task.completed)}><i className="fa fa-trash"></i></button></td>
+            </tr>);
+        });
+        let toDoTable = (
+          <table>
+            <thead>
+              <tr>
+                <th>Done?</th>
+                <th>Task</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>{toDoRows}</tbody>
+          </table>
+        )
+        let addTask = (
+          <div>
+            <h2>Add a new Task</h2>
+            <input name="task" type="text" placeholder="Let's go to the Mall" ref="task_entry"></input> <br></br>
+            <button type="button" name="submit" onClick={() => this.handleNewTask()}>Add Post</button>
+            <br></br> <br></br>
+          </div>
+        )
+        body = (
+          <div>
+          {toDoTable}
+          {addTask}
+          </div>
+        )
+      }
+      else body = (<h3>Loading...</h3>)
+      logout = (<button type="button" name="submit" onClick={()=>this.handleLogout()}>Logout</button>);
     }
     return (
       <div className="App">
